7572b2f9f14fd05365c456982286c156d417e2e2
Jukka Zitting <jukka@apache.org>
2008-09-14 11:16:43 +0000

    TIKA-120: Add support for retrieving ID3 tags from MP3 files
    Inline tag parsing into Mp3Parser class to simplify the code.
    
    Add a more specific test case.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tika/trunk@695191 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e97ee495b5007a01b872d00ba747f0d612cab72a

--- src/main/java/org/apache/tika/parser/mp3/Mp3Parser.java
+++ src/main/java/org/apache/tika/parser/mp3/Mp3Parser.java
@@ -19,6 +19,36 @@ end_package
 
 begin_import
 import|import
+name|java
+operator|.
+name|io
+operator|.
+name|IOException
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
+name|InputStream
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
+name|UnsupportedEncodingException
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -77,20 +107,6 @@ begin_import
 import|import
 name|org
 operator|.
-name|apache
-operator|.
-name|commons
-operator|.
-name|lang
-operator|.
-name|StringUtils
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
 name|xml
 operator|.
 name|sax
@@ -111,28 +127,8 @@ name|SAXException
 import|;
 end_import
 
-begin_import
-import|import
-name|java
-operator|.
-name|io
-operator|.
-name|IOException
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|io
-operator|.
-name|InputStream
-import|;
-end_import
-
 begin_comment
-comment|/**  *<p>  * The<code>Mp3Parser</code> is used to parse ID3 Version 1 Tag information  * from an MP3 file, if available.  *</p>  */
+comment|/**  * The<code>Mp3Parser</code> is used to parse ID3 Version 1 Tag information  * from an MP3 file, if available.  *  * @see http://www.id3.org/ID3v1  */
 end_comment
 
 begin_class
@@ -142,6 +138,262 @@ name|Mp3Parser
 extends|extends
 name|AbstractParser
 block|{
+comment|/**      * List of predefined genres.      *      * @see http://www.id3.org/id3v2-00      */
+specifier|private
+specifier|static
+specifier|final
+name|String
+index|[]
+name|GENRES
+init|=
+operator|new
+name|String
+index|[]
+block|{
+comment|/*  0 */
+literal|"Blues"
+block|,
+comment|/*  1 */
+literal|"Classic Rock"
+block|,
+comment|/*  2 */
+literal|"Country"
+block|,
+comment|/*  3 */
+literal|"Dance"
+block|,
+comment|/*  4 */
+literal|"Disco"
+block|,
+comment|/*  5 */
+literal|"Funk"
+block|,
+comment|/*  6 */
+literal|"Grunge"
+block|,
+comment|/*  7 */
+literal|"Hip-Hop"
+block|,
+comment|/*  8 */
+literal|"Jazz"
+block|,
+comment|/*  9 */
+literal|"Metal"
+block|,
+comment|/* 10 */
+literal|"New Age"
+block|,
+comment|/* 11 */
+literal|"Oldies"
+block|,
+comment|/* 12 */
+literal|"Other"
+block|,
+comment|/* 13 */
+literal|"Pop"
+block|,
+comment|/* 14 */
+literal|"R&B"
+block|,
+comment|/* 15 */
+literal|"Rap"
+block|,
+comment|/* 16 */
+literal|"Reggae"
+block|,
+comment|/* 17 */
+literal|"Rock"
+block|,
+comment|/* 18 */
+literal|"Techno"
+block|,
+comment|/* 19 */
+literal|"Industrial"
+block|,
+comment|/* 20 */
+literal|"Alternative"
+block|,
+comment|/* 21 */
+literal|"Ska"
+block|,
+comment|/* 22 */
+literal|"Death Metal"
+block|,
+comment|/* 23 */
+literal|"Pranks"
+block|,
+comment|/* 24 */
+literal|"Soundtrack"
+block|,
+comment|/* 25 */
+literal|"Euro-Techno"
+block|,
+comment|/* 26 */
+literal|"Ambient"
+block|,
+comment|/* 27 */
+literal|"Trip-Hop"
+block|,
+comment|/* 28 */
+literal|"Vocal"
+block|,
+comment|/* 29 */
+literal|"Jazz+Funk"
+block|,
+comment|/* 30 */
+literal|"Fusion"
+block|,
+comment|/* 31 */
+literal|"Trance"
+block|,
+comment|/* 32 */
+literal|"Classical"
+block|,
+comment|/* 33 */
+literal|"Instrumental"
+block|,
+comment|/* 34 */
+literal|"Acid"
+block|,
+comment|/* 35 */
+literal|"House"
+block|,
+comment|/* 36 */
+literal|"Game"
+block|,
+comment|/* 37 */
+literal|"Sound Clip"
+block|,
+comment|/* 38 */
+literal|"Gospel"
+block|,
+comment|/* 39 */
+literal|"Noise"
+block|,
+comment|/* 40 */
+literal|"AlternRock"
+block|,
+comment|/* 41 */
+literal|"Bass"
+block|,
+comment|/* 42 */
+literal|"Soul"
+block|,
+comment|/* 43 */
+literal|"Punk"
+block|,
+comment|/* 44 */
+literal|"Space"
+block|,
+comment|/* 45 */
+literal|"Meditative"
+block|,
+comment|/* 46 */
+literal|"Instrumental Pop"
+block|,
+comment|/* 47 */
+literal|"Instrumental Rock"
+block|,
+comment|/* 48 */
+literal|"Ethnic"
+block|,
+comment|/* 49 */
+literal|"Gothic"
+block|,
+comment|/* 50 */
+literal|"Darkwave"
+block|,
+comment|/* 51 */
+literal|"Techno-Industrial"
+block|,
+comment|/* 52 */
+literal|"Electronic"
+block|,
+comment|/* 53 */
+literal|"Pop-Folk"
+block|,
+comment|/* 54 */
+literal|"Eurodance"
+block|,
+comment|/* 55 */
+literal|"Dream"
+block|,
+comment|/* 56 */
+literal|"Southern Rock"
+block|,
+comment|/* 57 */
+literal|"Comedy"
+block|,
+comment|/* 58 */
+literal|"Cult"
+block|,
+comment|/* 59 */
+literal|"Gangsta"
+block|,
+comment|/* 60 */
+literal|"Top 40"
+block|,
+comment|/* 61 */
+literal|"Christian Rap"
+block|,
+comment|/* 62 */
+literal|"Pop/Funk"
+block|,
+comment|/* 63 */
+literal|"Jungle"
+block|,
+comment|/* 64 */
+literal|"Native American"
+block|,
+comment|/* 65 */
+literal|"Cabaret"
+block|,
+comment|/* 66 */
+literal|"New Wave"
+block|,
+comment|/* 67 */
+literal|"Psychadelic"
+block|,
+comment|/* 68 */
+literal|"Rave"
+block|,
+comment|/* 69 */
+literal|"Showtunes"
+block|,
+comment|/* 70 */
+literal|"Trailer"
+block|,
+comment|/* 71 */
+literal|"Lo-Fi"
+block|,
+comment|/* 72 */
+literal|"Tribal"
+block|,
+comment|/* 73 */
+literal|"Acid Punk"
+block|,
+comment|/* 74 */
+literal|"Acid Jazz"
+block|,
+comment|/* 75 */
+literal|"Polka"
+block|,
+comment|/* 76 */
+literal|"Retro"
+block|,
+comment|/* 77 */
+literal|"Musical"
+block|,
+comment|/* 78 */
+literal|"Rock& Roll"
+block|,
+comment|/* 79 */
+literal|"Hard Rock"
+block|,
+comment|/* sentinel */
+literal|""
+block|}
+decl_stmt|;
 specifier|public
 name|void
 name|parse
@@ -173,16 +425,6 @@ argument_list|,
 literal|"audio/mpeg"
 argument_list|)
 expr_stmt|;
-name|ID3v1Tag
-name|tag
-init|=
-name|ID3v1Tag
-operator|.
-name|createID3v1Tag
-argument_list|(
-name|stream
-argument_list|)
-decl_stmt|;
 name|XHTMLContentHandler
 name|xhtml
 init|=
@@ -199,45 +441,121 @@ operator|.
 name|startDocument
 argument_list|()
 expr_stmt|;
-if|if
-condition|(
-literal|null
-operator|!=
-name|tag
-condition|)
-block|{
-if|if
-condition|(
-name|StringUtils
-operator|.
-name|isNotEmpty
+name|byte
+index|[]
+name|buffer
+init|=
+name|getSuffix
 argument_list|(
-name|tag
-operator|.
-name|getTitle
-argument_list|()
-argument_list|)
-condition|)
-block|{
-name|xhtml
-operator|.
-name|element
-argument_list|(
-literal|"p"
+name|stream
 argument_list|,
-name|tag
-operator|.
-name|getTitle
-argument_list|()
+literal|128
 argument_list|)
-expr_stmt|;
-name|xhtml
+decl_stmt|;
+if|if
+condition|(
+name|buffer
 operator|.
-name|characters
+name|length
+operator|==
+literal|128
+operator|&&
+name|buffer
+index|[
+literal|0
+index|]
+operator|==
+literal|'T'
+operator|&&
+name|buffer
+index|[
+literal|1
+index|]
+operator|==
+literal|'A'
+operator|&&
+name|buffer
+index|[
+literal|2
+index|]
+operator|==
+literal|'G'
+condition|)
+block|{
+name|String
+name|title
+init|=
+name|getString
 argument_list|(
-literal|"\n"
+name|buffer
+argument_list|,
+literal|3
+argument_list|,
+literal|33
 argument_list|)
-expr_stmt|;
+decl_stmt|;
+name|String
+name|artist
+init|=
+name|getString
+argument_list|(
+name|buffer
+argument_list|,
+literal|33
+argument_list|,
+literal|63
+argument_list|)
+decl_stmt|;
+name|String
+name|album
+init|=
+name|getString
+argument_list|(
+name|buffer
+argument_list|,
+literal|63
+argument_list|,
+literal|93
+argument_list|)
+decl_stmt|;
+name|String
+name|year
+init|=
+name|getString
+argument_list|(
+name|buffer
+argument_list|,
+literal|93
+argument_list|,
+literal|97
+argument_list|)
+decl_stmt|;
+name|String
+name|comment
+init|=
+name|getString
+argument_list|(
+name|buffer
+argument_list|,
+literal|97
+argument_list|,
+literal|127
+argument_list|)
+decl_stmt|;
+name|int
+name|genre
+init|=
+operator|(
+name|int
+operator|)
+name|buffer
+index|[
+literal|127
+index|]
+operator|&
+literal|0xff
+decl_stmt|;
+comment|// unsigned byte
 name|metadata
 operator|.
 name|set
@@ -246,142 +564,7 @@ name|Metadata
 operator|.
 name|TITLE
 argument_list|,
-name|tag
-operator|.
-name|getTitle
-argument_list|()
-argument_list|)
-expr_stmt|;
-block|}
-if|if
-condition|(
-name|StringUtils
-operator|.
-name|isNotEmpty
-argument_list|(
-name|tag
-operator|.
-name|getArtist
-argument_list|()
-argument_list|)
-condition|)
-block|{
-name|xhtml
-operator|.
-name|element
-argument_list|(
-literal|"p"
-argument_list|,
-name|tag
-operator|.
-name|getArtist
-argument_list|()
-argument_list|)
-expr_stmt|;
-name|xhtml
-operator|.
-name|characters
-argument_list|(
-literal|"\n"
-argument_list|)
-expr_stmt|;
-block|}
-if|if
-condition|(
-name|StringUtils
-operator|.
-name|isNotEmpty
-argument_list|(
-name|tag
-operator|.
-name|getAlbum
-argument_list|()
-argument_list|)
-condition|)
-block|{
-name|xhtml
-operator|.
-name|element
-argument_list|(
-literal|"p"
-argument_list|,
-name|tag
-operator|.
-name|getAlbum
-argument_list|()
-argument_list|)
-expr_stmt|;
-name|xhtml
-operator|.
-name|characters
-argument_list|(
-literal|"\n"
-argument_list|)
-expr_stmt|;
-block|}
-if|if
-condition|(
-name|StringUtils
-operator|.
-name|isNotEmpty
-argument_list|(
-name|tag
-operator|.
-name|getYear
-argument_list|()
-argument_list|)
-condition|)
-block|{
-name|xhtml
-operator|.
-name|element
-argument_list|(
-literal|"p"
-argument_list|,
-name|tag
-operator|.
-name|getYear
-argument_list|()
-argument_list|)
-expr_stmt|;
-name|xhtml
-operator|.
-name|characters
-argument_list|(
-literal|"\n"
-argument_list|)
-expr_stmt|;
-block|}
-if|if
-condition|(
-name|StringUtils
-operator|.
-name|isNotEmpty
-argument_list|(
-name|tag
-operator|.
-name|getComment
-argument_list|()
-argument_list|)
-condition|)
-block|{
-name|xhtml
-operator|.
-name|element
-argument_list|(
-literal|"p"
-argument_list|,
-name|tag
-operator|.
-name|getComment
-argument_list|()
-argument_list|)
-expr_stmt|;
-name|xhtml
-operator|.
-name|characters
-argument_list|(
-literal|"\n"
+name|title
 argument_list|)
 expr_stmt|;
 name|metadata
@@ -390,47 +573,397 @@ name|set
 argument_list|(
 name|Metadata
 operator|.
-name|COMMENTS
+name|AUTHOR
 argument_list|,
-name|tag
-operator|.
-name|getComment
-argument_list|()
+name|artist
 argument_list|)
 expr_stmt|;
-block|}
-if|if
-condition|(
-name|StringUtils
+name|xhtml
 operator|.
-name|isNotEmpty
+name|element
 argument_list|(
-name|tag
-operator|.
-name|getGenreAsString
-argument_list|()
+literal|"h1"
+argument_list|,
+name|title
 argument_list|)
-condition|)
-block|{
+expr_stmt|;
+name|xhtml
+operator|.
+name|characters
+argument_list|(
+literal|"\n"
+argument_list|)
+expr_stmt|;
 name|xhtml
 operator|.
 name|element
 argument_list|(
 literal|"p"
 argument_list|,
-name|tag
-operator|.
-name|getGenreAsString
-argument_list|()
+name|artist
 argument_list|)
 expr_stmt|;
+name|xhtml
+operator|.
+name|characters
+argument_list|(
+literal|"\n"
+argument_list|)
+expr_stmt|;
+name|xhtml
+operator|.
+name|element
+argument_list|(
+literal|"p"
+argument_list|,
+name|album
+argument_list|)
+expr_stmt|;
+name|xhtml
+operator|.
+name|characters
+argument_list|(
+literal|"\n"
+argument_list|)
+expr_stmt|;
+name|xhtml
+operator|.
+name|element
+argument_list|(
+literal|"p"
+argument_list|,
+name|year
+argument_list|)
+expr_stmt|;
+name|xhtml
+operator|.
+name|characters
+argument_list|(
+literal|"\n"
+argument_list|)
+expr_stmt|;
+name|xhtml
+operator|.
+name|element
+argument_list|(
+literal|"p"
+argument_list|,
+name|comment
+argument_list|)
+expr_stmt|;
+name|xhtml
+operator|.
+name|characters
+argument_list|(
+literal|"\n"
+argument_list|)
+expr_stmt|;
+name|xhtml
+operator|.
+name|element
+argument_list|(
+literal|"p"
+argument_list|,
+name|GENRES
+index|[
+name|Math
+operator|.
+name|min
+argument_list|(
+name|genre
+argument_list|,
+name|GENRES
+operator|.
+name|length
+operator|-
+literal|1
+argument_list|)
+index|]
+argument_list|)
+expr_stmt|;
+name|xhtml
+operator|.
+name|characters
+argument_list|(
+literal|"\n"
+argument_list|)
+expr_stmt|;
+block|}
 name|xhtml
 operator|.
 name|endDocument
 argument_list|()
 expr_stmt|;
 block|}
+comment|/**      * Returns the identified ISO-8859-1 substring from the given byte buffer.      * The return value is the zero-terminated substring retrieved from      * between the given start and end positions in the given byte buffer.      * Extra whitespace (and control characters) from the beginning and the      * end of the substring is removed.      *      * @param buffer byte buffer      * @param start start index of the substring      * @param end end index of the substring      * @return the identified substring      * @throws TikaException if the ISO-8859-1 encoding is not available      */
+specifier|private
+specifier|static
+name|String
+name|getString
+parameter_list|(
+name|byte
+index|[]
+name|buffer
+parameter_list|,
+name|int
+name|start
+parameter_list|,
+name|int
+name|end
+parameter_list|)
+throws|throws
+name|TikaException
+block|{
+comment|// Find the zero byte that marks the end of the string
+name|int
+name|zero
+init|=
+name|start
+decl_stmt|;
+while|while
+condition|(
+name|zero
+operator|<
+name|end
+operator|&&
+name|buffer
+index|[
+name|zero
+index|]
+operator|!=
+literal|0
+condition|)
+block|{
+name|zero
+operator|++
+expr_stmt|;
 block|}
+comment|// Skip trailing whitespace
+name|end
+operator|=
+name|zero
+expr_stmt|;
+while|while
+condition|(
+name|start
+operator|<
+name|end
+operator|&&
+name|buffer
+index|[
+name|end
+operator|-
+literal|1
+index|]
+operator|<=
+literal|' '
+condition|)
+block|{
+name|end
+operator|--
+expr_stmt|;
+block|}
+comment|// Skip leading whitespace
+while|while
+condition|(
+name|start
+operator|<
+name|end
+operator|&&
+name|buffer
+index|[
+name|start
+index|]
+operator|<=
+literal|' '
+condition|)
+block|{
+name|start
+operator|++
+expr_stmt|;
+block|}
+comment|// Return the remaining substring
+try|try
+block|{
+return|return
+operator|new
+name|String
+argument_list|(
+name|buffer
+argument_list|,
+name|start
+argument_list|,
+name|end
+operator|-
+name|start
+argument_list|,
+literal|"ISO-8859-1"
+argument_list|)
+return|;
+block|}
+catch|catch
+parameter_list|(
+name|UnsupportedEncodingException
+name|e
+parameter_list|)
+block|{
+throw|throw
+operator|new
+name|TikaException
+argument_list|(
+literal|"ISO-8859-1 encoding is not available"
+argument_list|,
+name|e
+argument_list|)
+throw|;
+block|}
+block|}
+comment|/**      * Reads and returns the last<code>length</code> bytes from the      * given stream.      * @param stream input stream      * @param length number of bytes from the end to read and return      * @return stream the<code>InputStream</code> to read from.      * @throws IOException if the stream could not be read from.      */
+specifier|private
+specifier|static
+name|byte
+index|[]
+name|getSuffix
+parameter_list|(
+name|InputStream
+name|stream
+parameter_list|,
+name|int
+name|length
+parameter_list|)
+throws|throws
+name|IOException
+block|{
+name|byte
+index|[]
+name|buffer
+init|=
+operator|new
+name|byte
+index|[
+literal|2
+operator|*
+name|length
+index|]
+decl_stmt|;
+name|int
+name|bytesInBuffer
+init|=
+literal|0
+decl_stmt|;
+name|int
+name|n
+init|=
+name|stream
+operator|.
+name|read
+argument_list|(
+name|buffer
+argument_list|)
+decl_stmt|;
+while|while
+condition|(
+name|n
+operator|!=
+operator|-
+literal|1
+condition|)
+block|{
+name|bytesInBuffer
+operator|+=
+name|n
+expr_stmt|;
+if|if
+condition|(
+name|bytesInBuffer
+operator|==
+name|buffer
+operator|.
+name|length
+condition|)
+block|{
+name|System
+operator|.
+name|arraycopy
+argument_list|(
+name|buffer
+argument_list|,
+name|bytesInBuffer
+operator|-
+name|length
+argument_list|,
+name|buffer
+argument_list|,
+literal|0
+argument_list|,
+name|length
+argument_list|)
+expr_stmt|;
+name|bytesInBuffer
+operator|=
+name|length
+expr_stmt|;
+block|}
+name|n
+operator|=
+name|stream
+operator|.
+name|read
+argument_list|(
+name|buffer
+argument_list|,
+name|bytesInBuffer
+argument_list|,
+name|buffer
+operator|.
+name|length
+operator|-
+name|bytesInBuffer
+argument_list|)
+expr_stmt|;
+block|}
+if|if
+condition|(
+name|bytesInBuffer
+operator|<
+name|length
+condition|)
+block|{
+name|length
+operator|=
+name|bytesInBuffer
+expr_stmt|;
+block|}
+name|byte
+index|[]
+name|result
+init|=
+operator|new
+name|byte
+index|[
+name|length
+index|]
+decl_stmt|;
+name|System
+operator|.
+name|arraycopy
+argument_list|(
+name|buffer
+argument_list|,
+name|bytesInBuffer
+operator|-
+name|length
+argument_list|,
+name|result
+argument_list|,
+literal|0
+argument_list|,
+name|length
+argument_list|)
+expr_stmt|;
+return|return
+name|result
+return|;
 block|}
 block|}
 end_class
