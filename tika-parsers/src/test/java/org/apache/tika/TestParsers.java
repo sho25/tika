84494b777defb5c8e924e62ed3ad0296314410ff
Chris Mattmann <mattmann@apache.org>
2009-07-13 01:20:36 +0000

    - cleanup javadoc
    git-svn-id: https://svn.apache.org/repos/asf/lucene/tika/trunk@793441 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2be072649f2c90cf30217365ea6ef70f216a025d

--- tika-parsers/src/test/java/org/apache/tika/TestParsers.java
+++ tika-parsers/src/test/java/org/apache/tika/TestParsers.java
@@ -1032,7 +1032,7 @@ name|parser
 argument_list|)
 expr_stmt|;
 block|}
-comment|/**      * This method will give you back the filename incl. the absolute path name      * to the resource. If the resource does not exist it will give you back the      * resource name incl. the path.      *       * It will give you back an absolute path incl. the name which is in the      * same directory as the the class you've called it from.      *       * @param name      * @return      */
+comment|/**      * This method will give you back the filename incl. the absolute path name      * to the resource. If the resource does not exist it will give you back the      * resource name incl. the path.      *       * @param name      *            The named resource to search for.      * @return an absolute path incl. the name which is in the same directory as      *         the the class you've called it from.      */
 specifier|public
 name|String
 name|getFileResource
