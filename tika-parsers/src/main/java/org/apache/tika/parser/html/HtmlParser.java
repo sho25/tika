b927c06ce1f455e19568c8778ff02c7bd462409c
Jukka Zitting <jukka@apache.org>
2009-10-14 19:45:02 +0000

    TIKA-310: Use TagSoup to parse HTML
    git-svn-id: https://svn.apache.org/repos/asf/lucene/tika/trunk@825239 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d5090690acc7a353944f78c895cd7030cc34dce8

--- tika-parsers/src/main/java/org/apache/tika/parser/html/HtmlParser.java
+++ tika-parsers/src/main/java/org/apache/tika/parser/html/HtmlParser.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -244,20 +244,6 @@ operator|.
 name|xpath
 operator|.
 name|XPathParser
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|cyberneko
-operator|.
-name|html
-operator|.
-name|parsers
-operator|.
-name|SAXParser
 import|;
 end_import
 
@@ -742,11 +728,27 @@ argument_list|)
 argument_list|)
 expr_stmt|;
 comment|// Parse the HTML document
-name|SAXParser
+name|org
+operator|.
+name|ccil
+operator|.
+name|cowan
+operator|.
+name|tagsoup
+operator|.
+name|Parser
 name|parser
 init|=
 operator|new
-name|SAXParser
+name|org
+operator|.
+name|ccil
+operator|.
+name|cowan
+operator|.
+name|tagsoup
+operator|.
+name|Parser
 argument_list|()
 decl_stmt|;
 name|parser
