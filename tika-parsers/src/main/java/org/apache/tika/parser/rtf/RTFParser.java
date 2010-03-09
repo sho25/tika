889d964ebccb494ee12e2a10894b6ba3837dac69
Jukka Zitting <jukka@apache.org>
2010-03-09 22:55:31 +0000

    TIKA-386: Tika relies on X11
    Capture the reported NoClassDefFoundError and wrap it into a more appropriate TikaException
    
    git-svn-id: https://svn.apache.org/repos/asf/lucene/tika/trunk@921182 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 9a049b4cd60afdfa1eb37d58485758f8125b5340

--- tika-parsers/src/main/java/org/apache/tika/parser/rtf/RTFParser.java
+++ tika-parsers/src/main/java/org/apache/tika/parser/rtf/RTFParser.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -367,6 +367,49 @@ argument_list|,
 name|e
 argument_list|)
 throw|;
+block|}
+catch|catch
+parameter_list|(
+name|NoClassDefFoundError
+name|e
+parameter_list|)
+block|{
+if|if
+condition|(
+name|Boolean
+operator|.
+name|getBoolean
+argument_list|(
+literal|"java.awt.headless"
+argument_list|)
+condition|)
+block|{
+throw|throw
+operator|new
+name|TikaException
+argument_list|(
+literal|"Unexpected RTF parsing error in headless mode"
+argument_list|,
+name|e
+argument_list|)
+throw|;
+block|}
+else|else
+block|{
+throw|throw
+operator|new
+name|TikaException
+argument_list|(
+literal|"GUI environment expected by the javax.swing.text.rtf"
+operator|+
+literal|" package is not available, see JCR-386;"
+operator|+
+literal|" try running with -Djava.awt.headless=true"
+argument_list|,
+name|e
+argument_list|)
+throw|;
+block|}
 block|}
 block|}
 comment|/**      * @deprecated This method will be removed in Apache Tika 1.0.      */
