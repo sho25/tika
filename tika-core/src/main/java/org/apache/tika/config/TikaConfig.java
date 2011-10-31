2bae14802821291e2d5fce997e21ecd461953ff2
Jukka Zitting <jukka@apache.org>
2011-10-31 15:00:55 +0000

    Remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1195497 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 309a9a51ce315d70febd85e956ad06de4a416b5f

--- tika-core/src/main/java/org/apache/tika/config/TikaConfig.java
+++ tika-core/src/main/java/org/apache/tika/config/TikaConfig.java
@@ -101,16 +101,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|Map
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|Set
 import|;
 end_import
