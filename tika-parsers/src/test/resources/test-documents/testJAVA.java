74e22c9b4759f13800ece51f33b90c7d2b4e4cac
Hong-Thai Nguyen <thaichat04@apache.org>
2014-02-03 14:04:18 +0000

    TIKA-1224 - Adding a generic SourceCode parser for Java, Groovy, C++ with HTML render
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1563902 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 78a7ceb4e46d1e8b99533dc1261a42773ecf099d

---
+++ tika-parsers/src/test/resources/test-documents/testJAVA.java
@@ -0,0 +1,35 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*************************************************************************  *  Compilation:  javac HelloWorld.java  *  Execution:    java HelloWorld  *  *  Prints "Hello, World". By tradition, this is everyone's first program.  *  *************************************************************************/
+end_comment
+
+begin_class
+specifier|public
+class|class
+name|HelloWorld
+block|{
+specifier|public
+specifier|static
+name|void
+name|main
+parameter_list|(
+name|String
+index|[]
+name|args
+parameter_list|)
+block|{
+name|System
+operator|.
+name|out
+operator|.
+name|println
+argument_list|(
+literal|"Hello, World"
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+end_class
+
+end_unit
+
