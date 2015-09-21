502e3d5a6938b4117d8833f95c83898f5a142983
Konstantin Gribov <grossws@apache.org>
2015-09-21 18:17:12 +0000

    tika-example: explicit locale in String#toLowerCase
    git-svn-id: https://svn.apache.org/repos/asf/tika/trunk@1704379 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c31fe5533598aa155b521395d17b2932d6d4fe4e

--- tika-example/src/main/java/org/apache/tika/example/InterruptableParsingExample.java
+++ tika-example/src/main/java/org/apache/tika/example/InterruptableParsingExample.java
@@ -71,6 +71,16 @@ end_import
 
 begin_import
 import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Locale
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -364,7 +374,12 @@ name|length
 argument_list|)
 operator|.
 name|toLowerCase
+argument_list|(
+name|Locale
+operator|.
+name|getDefault
 argument_list|()
+argument_list|)
 argument_list|)
 expr_stmt|;
 if|if
