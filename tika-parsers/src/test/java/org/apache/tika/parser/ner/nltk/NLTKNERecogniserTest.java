7729e1e382d1aed813e72f18504f13ef08654966
manali <manalishah.91@gmail.com>
2016-02-19 17:37:25 -0800

    nltk modification
    Former-commit-id: 1b14b39d3e1b453620b2f7b26a933103a78c958a

--- tika-parsers/src/test/java/org/apache/tika/parser/ner/nltk/NLTKNERecogniserTest.java
+++ tika-parsers/src/test/java/org/apache/tika/parser/ner/nltk/NLTKNERecogniserTest.java
@@ -123,6 +123,16 @@ begin_import
 import|import
 name|java
 operator|.
+name|io
+operator|.
+name|File
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
 name|nio
 operator|.
 name|charset
