73a77ccdc1d39547a36fe337ff8de801f0139410
tballison <tallison@mitre.org>
2016-06-03 15:55:24 -0400

    TIKA-1508 add some unit tests for ParameterizedParserTest
    Former-commit-id: 18ab8f91f19b105270a107174afd64fe81d20f72

--- tika-core/src/test/java/org/apache/tika/parser/ConfigurableParserTest.java
+++ tika-core/src/test/java/org/apache/tika/parser/ConfigurableParserTest.java
@@ -131,16 +131,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|HashSet
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|Map
 import|;
 end_import
