a769e5a7bc35e70bfced7bb546ff3dabe38c54a7
ben <ben.romberg@gmx.de>
2018-01-01 22:32:54 +0100

    fix for TIKA-1191 contributed by BenRomberg
    Former-commit-id: 6a398bd3f6245543091fd7c0e9e4facb34a26882

---
+++ tika-core/src/test/java/org/apache/tika/fork/unusedpackage/ClassInUnusedPackage.java
@@ -0,0 +1,24 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|tika
+operator|.
+name|fork
+operator|.
+name|unusedpackage
+package|;
+end_package
+
+begin_class
+specifier|public
+class|class
+name|ClassInUnusedPackage
+block|{ }
+end_class
+
+end_unit
+
