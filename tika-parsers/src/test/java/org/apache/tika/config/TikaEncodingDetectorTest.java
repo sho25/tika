b6aef0f98e2c64a03bc218613590cd7ed275b37b
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2018-05-01 18:55:35 -0700

    TIKA-2636 ENVI Header metadata fields can span more than one line
    Former-commit-id: 95d967b14c33acd4e82814f6cb470cbab0f4ee08

--- tika-parsers/src/test/java/org/apache/tika/config/TikaEncodingDetectorTest.java
+++ tika-parsers/src/test/java/org/apache/tika/config/TikaEncodingDetectorTest.java
@@ -854,7 +854,7 @@ argument_list|)
 expr_stmt|;
 name|assertEquals
 argument_list|(
-literal|3
+literal|4
 argument_list|,
 name|parsers
 operator|.
@@ -1035,7 +1035,7 @@ argument_list|)
 expr_stmt|;
 name|assertEquals
 argument_list|(
-literal|3
+literal|4
 argument_list|,
 name|parsers
 operator|.
@@ -1209,7 +1209,7 @@ argument_list|)
 expr_stmt|;
 name|assertEquals
 argument_list|(
-literal|3
+literal|4
 argument_list|,
 name|parsers
 operator|.
