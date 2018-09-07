0bb25d479533c546d68993ac5592351825910017
TALLISON <tallison@apache.org>
2018-09-07 19:16:32 -0400

    TIKA-2725 -- checkpoint commit ... basic child process is started...need to integrate actual statuswatcher, etc.
    # Conflicts:
    #	tika-parsers/src/test/resources/test-documents/testPST.pst
    #	tika-parsers/src/test/resources/test-documents/testPST_variousBodyTypes.pst
    
    
    Former-commit-id: bf1b2418d47e9fa47e0634825341921d7edd1ee8

---
+++ tika-server/src/main/java/org/apache/tika/server/FileCountExceededException.java
@@ -0,0 +1,28 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|tika
+operator|.
+name|server
+package|;
+end_package
+
+begin_comment
+comment|/**  * Exception thrown by ServerStatusWatcher if tika-server exceeds  * the maximum number of files to process.  */
+end_comment
+
+begin_class
+specifier|public
+class|class
+name|FileCountExceededException
+extends|extends
+name|Exception
+block|{  }
+end_class
+
+end_unit
+
